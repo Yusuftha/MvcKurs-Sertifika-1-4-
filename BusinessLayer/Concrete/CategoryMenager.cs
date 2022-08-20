5f0106cea032de86479c9148e2000cc1aed20e3c
Yusuf Taha Aydemir <yuta.aydemir@gmail.com>
2022-08-20 10:08:06 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryMenager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccesLayer.Concrete.Repositories;
+using EntitiyLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryMenager
+    {
+        GenericRopository<Category> repo = new GenericRopository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoriyAddBL(Category p)
+        {
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51) 
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repo.Insert(p);
+            }
+        }
+    }
+}
